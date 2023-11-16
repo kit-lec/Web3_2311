7b8da418163d5ac7d5c4bf48acb7430e9a732e8a
kit-lec <kit_lec@hotmail.com>
2023-11-16 21:49:14 +0900

    231116_4

--- SpringStudy/Bt15_MyBatis/src/main/java/com/lec/spring/controller/BoardController.java
+++ SpringStudy/Bt15_MyBatis/src/main/java/com/lec/spring/controller/BoardController.java
@@ -1,7 +1,13 @@
 package com.lec.spring.controller;
 
+import com.lec.spring.domain.Post;
+import com.lec.spring.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // Controller layer
@@ -10,14 +16,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/write")   // /board/write
     public void write(){}
 
-    @GetMapping("/detail")
-    public void detail(){}
+    @PostMapping("/write")
+    public String writeOk(Post post, Model model){
+        model.addAttribute("result", boardService.write(post));
+        return "board/writeOk";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable  Long id, Model model){
+        model.addAttribute("post", boardService.detail(id));
+        return "board/detail";
+    }
 
     @GetMapping("/list")
-    public void list(){}
+    public void list(Model model){
+        model.addAttribute("list", boardService.list());
+    }
 
     @GetMapping("/update")
     public void update(){}

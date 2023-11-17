7e96cb5978db2bb5846817e4660c4c034cf47c74
kit-lec <kit_lec@hotmail.com>
2023-11-17 20:52:36 +0900

    231117_2

--- SpringStudy/Bt15_MyBatis/src/main/java/com/lec/spring/controller/BoardController.java
+++ SpringStudy/Bt15_MyBatis/src/main/java/com/lec/spring/controller/BoardController.java
@@ -1,14 +1,19 @@
 package com.lec.spring.controller;
 
 import com.lec.spring.domain.Post;
+import com.lec.spring.domain.PostValidator;
 import com.lec.spring.service.BoardService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 // Controller layer
 //  request 처리 ~ response
@@ -23,7 +28,32 @@ public class BoardController {
     public void write(){}
 
     @PostMapping("/write")
-    public String writeOk(Post post, Model model){
+    public String writeOk(
+            @Valid Post post
+            , BindingResult result    // <- Validator 가 유효성 검사를 한 결과가 담긴 객체
+            , Model model   // BindingResult 보다  Model 을 뒤에 선언
+            , RedirectAttributes redirectAttrs    // redirect 시 넘겨줄 값들
+    ){
+        // validation 에러가 있었다면 redirect 한다!
+        if(result.hasErrors()){
+
+            // addAttribute
+            //    request parameters로 값을 전달.  redirect URL에 query string 으로 값이 담김
+            //    request.getParameter에서 해당 값에 액세스 가능
+            // addFlashAttribute
+            //    일회성. 한번 사용하면 Redirect후 값이 소멸
+            //    request parameters로 값을 전달하지않음
+            //    '객체'로 값을 그대로 전달
+
+
+            List<FieldError> errList = result.getFieldErrors();
+            for(FieldError err : errList){
+                redirectAttrs.addFlashAttribute("error_" + err.getField(), err.getCode());
+            }
+
+            return "redirect:/board/write";
+        }
+
         model.addAttribute("result", boardService.write(post));
         return "board/writeOk";
     }
@@ -55,6 +85,13 @@ public class BoardController {
     public String deleteOk(Long id, Model model){
         model.addAttribute("result", boardService.deleteById(id));
         return "board/deleteOk";
+    }
+
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        System.out.println("initBinder() 호출");
+        binder.setValidator(new PostValidator());
     }
 
 }

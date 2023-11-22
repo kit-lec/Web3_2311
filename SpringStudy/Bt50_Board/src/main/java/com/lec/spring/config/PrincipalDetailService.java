6a2fb1b0fcaa50df03b55f43270fa49342a9499e
kit-lec <kit_lec@hotmail.com>
2023-11-22 19:52:24 +0900

    231122_1

--- SpringStudy/Bt50_Board/src/main/java/com/lec/spring/config/PrincipalDetailService.java
+++ SpringStudy/Bt50_Board/src/main/java/com/lec/spring/config/PrincipalDetailService.java
@@ -1,5 +1,9 @@
 package com.lec.spring.config;
 
+import com.lec.spring.domain.User;
+import com.lec.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,12 +18,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PrincipalDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("loadUserByUsername(" + username + ") 호출");
 
-        // TODO
+        // DB 조회
+        User user = userService.findByUsername(username);
 
-        return null;
+        // 해당 username 의 user 가  DB 에 있다면
+        // UserDetails 생성해서 리턴
+        if(user != null){
+            PrincipalDetails userDetails = new PrincipalDetails(user);
+            userDetails.setUserService(userService);
+            return userDetails;
+        }
+
+        // 해당 username 의 user 가 DB 에 없다면?
+        throw new UsernameNotFoundException(username);
+
+        //return null;   // 여기서 null 리턴하면 예외 발생
     }
 }
+
+
+
+
+
+
+
+
+
--- SpringStudy/Bt50_Board/src/main/java/com/lec/spring/config/PrincipalDetails.java
+++ SpringStudy/Bt50_Board/src/main/java/com/lec/spring/config/PrincipalDetails.java
@@ -1,11 +1,14 @@
 package com.lec.spring.config;
 
+import com.lec.spring.domain.Authority;
 import com.lec.spring.domain.User;
 import com.lec.spring.service.UserService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 //시큐리티가 /user/login (POST) 주소요청이 오면 낚아채서 로그인을 진행시킨다.
 //로그인(인증) 진행이 완료되면 '시큐리티 session' 에 넣어주게 된다.
@@ -31,40 +34,74 @@ public class PrincipalDetails implements UserDetails {
     // 로그인한 사용자 정보
     private User user;
 
-    // TODO
+    public User getUser() {
+        return user;
+    }
 
+    public PrincipalDetails(User user){
+        System.out.println("UserDetails(user) 생성: " + user);
+        this.user = user;
+    }
+
+    // 해당 User 의 '권한(들)'을 리턴
+    // 현재 로그인한 사용자의 권한정보가 필요할때마다 호출된다. 혹은 필요할때마다 직접 호출해 사용할수도 있다
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        System.out.println("getAuthorities() 호출");
+
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+
+        List<Authority> list = userService.selectAuthoritiesById(user.getId());   // DB 에서 user 의 권한(들)을 읽어오기
+
+        for(Authority auth : list){
+            collect.add(new GrantedAuthority() {
+                @Override
+                public String getAuthority() {
+                    return auth.getName();
+                }
+
+                // thymeleaf 등에서 활용할 목적 (학습목적)
+                @Override
+                public String toString() {
+                    return auth.getName();
+                }
+            });
+        }
+
+        return collect;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUsername();
     }
 
+    // 계정이 만료되짆 않았는지?
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
+    // 계정이 잠긴건 아닌지?
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
+    // 계정 credential 이 만료된건 아닌지
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
+    // 계정이 활성화 되었는지?
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }

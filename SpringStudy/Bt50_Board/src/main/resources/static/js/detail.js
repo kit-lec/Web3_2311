1a0b57425265089d9b3fcc10dcfbe63072220169
kit-lec <kit_lec@hotmail.com>
2023-11-28 21:51:27 +0900

    231128_3

--- SpringStudy/Bt50_Board/src/main/resources/static/js/detail.js
+++ SpringStudy/Bt50_Board/src/main/resources/static/js/detail.js
@@ -13,6 +13,46 @@ $(function(){
     // 현재 글의 댓글을 불러온다
     loadComment(id);
 
+	// 댓글 작성 버튼 누르면 댓글 등록 하기.
+	// 1. 어느글에 대한 댓글인지? --> 위에 id 변수에 담겨있다
+	// 2. 어느 사용자가 작성한 댓글인지? --> logged_id 값
+	// 3. 댓글 내용은 무엇인지?  --> 아래 content
+    $("#btn_comment").click(function(){
+        // 입력한 댓글
+        const content = $("#input_comment").val().trim();
+
+        // 검증
+        if(!content){
+            alert("댓글 입력을 하세요");
+            $("#input_comment").focus();
+            return;
+        }
+
+        // 전달할 parameter 들 준비
+        const data = {
+            "post_id": id,
+            "user_id": logged_id,
+            "content": content,
+        };
+
+        $.ajax({
+            url: "/comment/write",
+            type: "POST",
+            data: data,
+            cache: false,
+            success: function(data, status){
+                if(status == "success"){
+                    if(data.status !== "OK"){
+                        alert(data.status);
+                        return;
+                    }
+                    loadComment(id);   // 댓글 목록 다시 업데이트
+                    $("#input_comment").val('');   // 입력칸 리셋
+                }
+            },
+        });
+
+    });
 });
 
 // 특정 글 (post_id) 의 댓글 목록 읽어오기
@@ -31,7 +71,8 @@ function loadComment(post_id){
 
                 buildComment(data);   // 댓글 화면 렌더링
 
-                // TODO
+                // ★ 댓글목록을 불러오고 난 뒤에 삭제에 대한 이벤트 리스너를 등록해야 한다
+                addDelete();
             }
         },
     });
@@ -49,18 +90,67 @@ function buildComment(result){
         let username = comment.user.username;
         let name = comment.user.name;
 
-        const delBtn = `<i class="btn fa-solid fa-delete-left text-danger" data-bs-toggle="tooltip" title="삭제"></i>`;
+        const delBtn = (logged_id !== user_id) ? '' : `
+            <i class="btn fa-solid fa-delete-left text-danger" data-bs-toggle="tooltip"
+                data-cmtdel-id="${id}" title="삭제"></i>
+            `;
         const row = `
             <tr>
-                <td><span><strong>USER1</strong><br><small class="text-secondary">(사용자1)</small></span></td>
+                <td><span><strong>${username}</strong><br><small class="text-secondary">(${name})</small></span></td>
                 <td>
-                    <span>정말 정말 재미 있습니다.</span>
+                    <span>${content}</span>${delBtn}
                 </td>
-                <td><span><small class="text-secondary">2022.03.22 13:43:05</small></span></td>
+                <td><span><small class="text-secondary">${regdate}</small></span></td>
             </tr>
             `;
+        out.push(row);
+    });
+
+    $("#cmt_list").html(out.join("\n"));
+
+}
+
+// 댓글 삭제 버튼이 눌렸을때, 해당 댓글 삭제하는 이벤트를 삭제버튼에 등록
+function addDelete(){
+
+    // 현재 글의 id (댓글 삭제후에도 댓글 목록 갱신 필요)
+    const id = $("input[name='id']").val().trim();
+
+    $("[data-cmtdel-id]").click(function(){
+        if(!confirm("댓글을 삭제하시겠습니까?")) return;
+
+        // 삭제할 댓글의 comment_id
+        const comment_id = $(this).attr("data-cmtdel-id");
+
+        $.ajax({
+            url: "/comment/delete",
+            type: "POST",
+            cache: false,
+            data: {"id": comment_id},
+            success: function(data, status){
+                if(status == "success"){
+                    if(data.status !== "OK"){
+                        alert(data.status);
+                        return;
+                    }
+
+                    // 삭제후에도 다시 댓글 목록 불러와야 한다.
+                    loadComment(id);
+                }
+            },
+        });
     });
 
 }
+
+
+
+
+
+
+
+
+
+
 
 

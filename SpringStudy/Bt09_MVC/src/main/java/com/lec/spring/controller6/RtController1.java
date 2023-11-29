d9f950d93f9c4e09c9768571b31af04a2bfc525a
kit-lec <kit_lec@hotmail.com>
2023-11-29 20:56:37 +0900

    231129_2

--- SpringStudy/Bt09_MVC/src/main/java/com/lec/spring/controller6/RtController1.java
+++ SpringStudy/Bt09_MVC/src/main/java/com/lec/spring/controller6/RtController1.java
@@ -1,7 +1,12 @@
 package com.lec.spring.controller6;
 
+import com.lec.spring.domain.User;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -60,11 +65,40 @@ import java.net.URI;
  *
  */
 
+/**
+ *  org.springframework.http 패키지의
+ *
+ *  HttpEntity<T> :
+ *      │          HTTP request, 혹은 response entity 표현객체. 'headers' 와 'body' 로 구성됨
+ *      │          SpringMVC @Controller 메소드의 return 값으로도 사용
+ *      │          RestTemplate 에서도 활용
+ *      │          https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpEntity.html
+ *      │
+ *      └─ RequestEntity<T>
+ *      │          HTTP request 표현객체
+ *      │          SpringMVC @Controller 메소드의 입력값, RestTemplate 에서 활용
+ *      │          https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/RequestEntity.html
+ *      │
+ *      └─ ResponseEntity<T>
+ *                 HTTP response 표현객체
+ *                 SpringMVC @Controller 메소드의 리턴값, RestTemplate 에서 활용
+ *                 https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html
+ *
+ */
 
 @RestController
 public class RtController1 {
 
     public static final String HTTPBIN_URL = "https://httpbin.org";
+
+    @Value("${app.api-key.seoul-data}")
+    private String seoul_data_key;
+    @Value("${app.api-key.kobis}")
+    private String kobis_key;
+    @Value("${app.api-key.korean-stddic}")
+    private String korean_stddic_key;
+
+
 
     @RequestMapping("api/test01")
     public String apiTest01(){
@@ -181,7 +215,110 @@ public class RtController1 {
         return rt.getForObject(uri, String.class);
     }
 
+    //────────────────────────────────────────────────────────────────
+    // Path Variable 을 활용한 요청
+    // 서울시 공공데이터 - 지하철 승하차 정보
+    //   샘플 url : http://openapi.seoul.go.kr:8088/4d46796d7366726f3833774a774955/json/CardSubwayStatsNew/1/5/20181001
+    @RequestMapping("api/test06/{start_index}/{end_index}/{date}")
+    public ResponseEntity<String> apiTest06(
+            @PathVariable("start_index") Integer startIndex,
+            @PathVariable("end_index") Integer endIndex,
+            @PathVariable("date") String date
+    ){
 
+//        String uri = String.format("http://openapi.seoul.go.kr:8088/%s/json/CardSubwayStatsNew/%d/%d/%s"
+//                            , seoul_data_key, startIndex, endIndex, date);
+
+        String baseUri = "http://openapi.seoul.go.kr:8088/{key}/json/CardSubwayStatsNew/{startIndex}/{endIndex}/{date}";
+        // expand() 를 사용하여 "{ .. }" URI template variable 들을 주어진 값으로 차례대로 치환
+        //   .expand(Object... uriVariableValues)
+        //   .expand(Map<String,?> uriVariables)
+        //   .expand(UriComponents.UriTemplateVariables uriVariables)
+
+        URI uri = UriComponentsBuilder
+                .fromUriString(baseUri)
+                .build()
+                .expand(seoul_data_key, startIndex, endIndex, date)
+                .toUri();
+
+
+        RestTemplate rt = new RestTemplate();
+        String result = rt.getForObject(uri, String.class);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result);
+    }
+
+    // 영화진흥위원회 - 영화목록조회
+    @RequestMapping("api/test07")
+    public ResponseEntity<String> apiTest07(String movieNm, Integer openStartDt, Integer itemPerPage, Integer curPage){
+        String baseUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString(baseUrl)
+                .queryParam("key", kobis_key)
+                .queryParam("movieNm", movieNm)
+                .queryParam("openStartDt", openStartDt)
+                .queryParam("itemPerPage", itemPerPage)
+                .queryParam("curPage", curPage)
+                .build()
+                .encode()
+                .toUri();
+
+        String result = new RestTemplate().getForObject(uri, String.class);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result);
+    }
+
+    // ex) http://localhost:8080/api/test07?movieNm=미션&openStartDt=2000&itemPerPage=5&curPage=3
+
+    //--------------------------------------------------------------
+    // POST 요청
+    @RequestMapping("api/test10")
+    public String apiTest10(){
+        URI uri = UriComponentsBuilder
+                .fromUriString(HTTPBIN_URL)
+                .path("/post")
+                .build()
+                .toUri();
+
+        RestTemplate rt = new RestTemplate();
+        String result = null;
+        // postForObject()!  post 방식으로
+        //   postForObject(String url, Object request, Class<T> responseType, Object... uriVariables)
+        //   postForObject(String url, Object request, Class<T> responseType, Map<String, ?> uriVariables)
+        //   postForObject(URI, Object request, Class<T> responseType)
+
+        //result = rt.postForObject(uri, null, String.class);
+
+        User user = new User(34, "홍길동");
+        result = rt.postForObject(uri, user, String.class);
+
+        return result;
+    }
+    
+    //-------------------------------------------
+    // exchange 사용
+    @RequestMapping("api/test21")
+    public ResponseEntity<String> apiTest21(){
+        User user = new User(26, "김봉남");
+
+        RequestEntity<User> requestEntity = RequestEntity
+                .post(HTTPBIN_URL + "/post")
+                //.contentType(MediaType.APPLICATION_JSON)
+                .header("Content-type", "application/json")
+                .header("x-auth", "abcd")
+                .header("x-key", "1234")
+                .body(user)
+                ;
+
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> result = rt.exchange(requestEntity, String.class);
+
+        return result;
+    }
 
 }
 
